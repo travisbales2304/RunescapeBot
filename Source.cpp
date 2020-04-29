f44cd720c71b73afcd5c70dafe2cfd4ba844b08e
Travis <gligglebuck@yahoo.com>
2020-04-28 20:36:59 -0500

    added main menu and menu options

--- Source.cpp
+++ Source.cpp
@@ -3,21 +3,52 @@ using namespace std;
 
 
 
-void function1();
+void FunctionMenu();
+void Title();
+void MenuText();
 
 int main()
 {
-	function1();
-	cout << "suck my ass" << endl;
+	FunctionMenu();
 }
 
-void function1()
+void FunctionMenu()
 {
 	int x = 0;
-
-
-	if (x == 0)
+	Title();
+	MenuText();
+	cin >> x;
+	switch (x)
 	{
-		cout << "balls bro" << endl;
+	case 1:
+	{
+		break;
 	}
+	case 2:
+	{
+		break;
+	}
+	case 3:
+	{
+		break;
+	}
+	case 4: 
+	{
+		break;
+	}
+
+	}
+
+}
+
+
+void MenuText()
+{
+	cout << "Main Menu" << endl;
+	cout << "1.) Woocutter" << endl << "2.) Miner" << endl << "3.) Fighter" << endl << "4.) Settings" << endl;
+}
+
+void Title()
+{
+	cout << "RSBOT --- Created by Dustin and Travis" << endl << endl;
 }
